236608f4b82dc78b86f87b860bfb7781462ef320
nidiyan <nidiyan@gmail.com>
2023-06-17 13:30:56 -0500

    init

---
+++ nids-first-app/nids-first-app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  nids-first-app
+//
+//  Created by Nidiyan Rajendran on 6/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nids-first-app/nids-first-app/configs/constants.swift
@@ -0,0 +1,14 @@
+//
+//  constants.swift
+//  nids-first-app
+//
+//  Created by Nidiyan Rajendran on 6/17/23.
+//
+
+import Foundation
+
+struct constants {
+    struct urls {
+        static let stock_api = "https://tradestie.com/api/v1/apps/reddit";
+    }
+}
